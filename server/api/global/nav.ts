1dff5b804e53d882c1620824cec2ffb3f98af2d0
daidr <daidr@daidr.me>
2023-01-22 16:00:05 +0800

    feat(api): add `/api/global/nav`

---
+++ server/api/global/nav.ts
@@ -0,0 +1,31 @@
+import { apiResponse } from '~~/server/utils'
+
+interface INavItem {
+  id: number
+  label: string
+  badge: string | null
+  link: string
+}
+
+interface IOriginNavResponse {
+  data: {
+    id: number
+    attributes: {
+      nav: INavItem[]
+      siteName: string
+      createdAt: string
+      updatedAt: string
+      publishedAt: string
+    }
+  }
+}
+
+const { apiEntry } = useRuntimeConfig()
+
+export default defineEventHandler(async () => {
+  const result: IOriginNavResponse = await $fetch('/api/global?populate=nav', {
+    baseURL: apiEntry,
+  })
+
+  return apiResponse<INavItem[]>(result.data.attributes.nav)
+})
