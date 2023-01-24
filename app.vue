fa524b7e6aaf4353158b5e216617fad183069073
daidr <daidr@daidr.me>
2023-01-24 17:55:00 +0800

    feat: favicon support

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+const { data: meta } = await useFetch('/api/global/meta')
+
+useHead({
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: meta.value!.data.favicon,
+    },
+  ],
+})
+
+provide('meta', meta.value!.data)
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
