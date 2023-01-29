6b3b0f96d5879545542db8a3cef68bc6e59c2350
LinHaoBin <1311902424@qq.com>
2023-01-29 17:02:23 +0800

    feat: 测试文章列表

---
+++ components/ArticleList/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="article-list">
+    <ul class="tab">
+      <li v-for="k in arr" :key="k" class="tab-item">{{ k }}</li>
+    </ul>
+    <div class="list">
+      <div class="item"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const arr: string[] = ['推荐', '最新', '热榜']
+</script>
+
+<style lang="scss" scoped>
+.article-list {
+  @apply w-full md:w-700px;
+  background: pink;
+  .tab {
+    @apply w-60 grid grid-cols-3 divide-x;
+    .tab-item {
+      @apply text-center;
+      &.tab-active {
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@ useHead({
 <template>
   <div>
     <p>TODO: 首页</p>
+    <ArticleList></ArticleList>
     <p v-for="i of Array.from({ length: 100 }, (_, i) => i + 1)" :key="i">
       用于测试滚动的文字
     </p>
