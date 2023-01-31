5432f0661bc0d1f569c024750a513330e30ad166
daidr <daidr@daidr.me>
2023-01-31 10:44:55 +0800

    feat: 为header添加阴影

--- pages/index.vue
+++ pages/index.vue
@@ -58,6 +58,7 @@ const isHeaderVisible = inject('isHeaderVisible') as Ref<boolean>
     @apply border-b-1 border-gray-200 dark:border-[#494949];
     @apply fixed top-0 z-11;
     @apply transform-gpu transition-transform;
+    @apply shadow-sm shadow-black/5;
 
     &.visible {
       @apply translate-y-50px sm:translate-y-60px;
