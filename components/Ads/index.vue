19e8e2c3b0677025c3875b8fb05c19adc2279002
daidr <daidr@daidr.me>
2023-02-01 22:28:32 +0800

    feat: ads support

--- components/Ads/index.vue
+++ components/Ads/index.vue
@@ -1,20 +1,52 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { IMeta } from '~~/server/api/global/meta'
+
+const meta = inject('meta') as IMeta
+</script>
 
 <template>
-  <div class="buy_img">
+  <div class="ads-wrapper">
     <a
-      href="https://juejin.cn/pin/7190294399614451773?utm_source=webbanner&utm_medium=banner&utm_campaign=xcnewyear"
-      type="_blank"
+      v-for="ad of meta.ads"
+      :key="ad.id"
+      :href="ad.link"
+      target="_blank"
+      :title="ad.summary"
     >
-      <img
-        src="https://p6-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/4023ed80e2794fb48858b4809b17f139~tplv-k3u1fbpfcp-no-mark:480:400:0:0.awebp?"
-        alt=""
-      />
+      <div class="ad">
+        <img :src="ad.thumbnail" alt="" />
+      </div>
     </a>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.buy_img {
+.ads-wrapper {
+  // 这里应该用方向为 column 的 flex 布局
+  // 但是 flex 布局的 gap 兼容性不好，所以用 grid 布局代替了
+  @apply grid grid-cols-1 gap-y-4.2;
+
+  .ad,
+  .ad img {
+    @apply w-240px h-200px rounded-sm;
+  }
+
+  .ad {
+    @apply shadow-sm shadow-black/5 dark:shadow-white/5;
+    @apply relative;
+
+    img {
+      @apply object-cover;
+    }
+
+    &::after {
+      content: '广告';
+      @apply opacity-70;
+      @apply absolute bottom-2.5 right-2.5;
+      @apply rounded-md border-white border-1 px-2;
+      @apply text-base text-white bg-black/40;
+      @apply transform-gpu scale-70 origin-bottom-right;
+    }
+  }
 }
 </style>
