4353609beb2711a8948cc3e761b474994fdb053b
daidr <daidr@daidr.me>
2023-02-08 16:49:37 +0800

    feat: add writer card

---
+++ components/WriterCard/index.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { IWriter } from '~~/server/api/global/meta'
+
+defineProps<{
+  writer: IWriter
+}>()
+</script>
+
+<template>
+  <div class="writercard-wrapper">
+    <img :src="writer.avatar" alt="" />
+    <div class="details">
+      <div class="name">{{ writer.name }}</div>
+      <div class="motto">{{ writer.motto }}</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.writercard-wrapper {
+  @apply w-full rounded-4px;
+  @apply bg-white dark:bg-[#121212];
+  @apply p-5 flex items-center;
+
+  img {
+    @apply w-12 h-12 rounded-full;
+  }
+
+  .details {
+    @apply ml-4;
+    @apply flex flex-col;
+
+    .name {
+      @apply max-w-180px text-lg;
+      @apply overflow-ellipsis whitespace-nowrap overflow-hidden;
+    }
+
+    .motto {
+      @apply text-[#515767];
+    }
+  }
+}
+</style>
