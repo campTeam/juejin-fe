e0879048fec91666b420550b8dfd14c6d5a6053f
daidr <daidr@daidr.me>
2023-02-08 19:44:43 +0800

    feat: article sidebar wrapper

---
+++ components/ArticleSideItem/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="article-side-wrapper">
+    <div class="article-side-title">
+      <slot name="title"></slot>
+    </div>
+    <div class="article-side-content">
+      <slot name="default"></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.article-side-wrapper {
+  @apply rounded-4px w-300px;
+  @apply bg-white dark:bg-[#121212];
+
+  .article-side-title {
+    @apply mx-20px py-16px;
+    @apply text-base leading-2rem;
+    @apply text-black dark:text-white;
+    @apply border-b-1 border-gray-200 dark:border-[#494949];
+  }
+
+  .article-side-content {
+    @apply max-h-460px relative;
+    @apply mt-8px mr-4px overflow-auto;
+
+    &::-webkit-scrollbar {
+      @apply w-6px h-80px;
+    }
+
+    &::-webkit-scrollbar-track {
+      @apply shadow-none rounded-2px;
+    }
+
+    &::-webkit-scrollbar-thumb {
+      @apply outline-none rounded-2px bg-[#e4e6eb] dark:bg-[#2c2e31];
+    }
+  }
+}
+</style>
