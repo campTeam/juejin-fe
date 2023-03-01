779299c3e52f143c684ec832cf85c050c7bb16d6
戴兜~ <daidr@daidr.me>
2023-03-01 14:03:55 +0800

    Update useTimeAgoCN.ts

--- composables/useTimeAgoCN.ts
+++ composables/useTimeAgoCN.ts
@@ -11,7 +11,7 @@ export const useTimeAgoCN = (
     justNow: '刚刚',
     past: n => (n.match(/\d/) ? `${n}前` : n),
     future: n => (n.match(/\d/) ? `${n}后` : n),
-    month: (n, past) => (n === 1 ? (past ? '下个月' : '上个月') : `${n}个月`),
+    month: (n, past) => (n === 1 ? (past ? '上月' : '下月') : `${n}月`),
     year: (n, past) => (n === 1 ? (past ? '去年' : '明年') : `${n}年`),
     day: (n, past) => (n === 1 ? (past ? '昨天' : '明天') : `${n}天`),
     week: (n, past) => (n === 1 ? (past ? '上周' : '下周') : `${n}周`),
